e540e558e1ed4f1ea05d56817691e3fc43704223
Atul.Gaurav <aatul.gaurav@gmail.com>
2022-05-04 21:30:14 +0530

    Initial commit

---
+++ src/main/java/com/example/springbootsecurity/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.springbootsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //set your configuration on the auth object
+        auth.inMemoryAuthentication()
+                .withUser("blah")
+                .password("blah")
+                .roles("USER")
+                .and()
+                .withUser("foo")
+                .password("foo")
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //start the path matchers from a specific one at the beginning with general one at bottom
+        // /** means all root and sub root paths
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN") //admin api accessile by ADMIN role only
+                //.antMatchers("/user").hasRole("USER")// to facilitate user api to be accessible by single role
+                .antMatchers("/user").hasAnyRole("USER","ADMIN") //user api can be accessed by both roles            .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
